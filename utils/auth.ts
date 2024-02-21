dbfd6eed0a4f90f8db98084392a7d56385f652a1
Tyronwilliam <tyronwillianchanu@gmail.com>
2024-02-21 12:37:23 +0100

    hide empty competence + Sort talent lyout

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,5 +1,4 @@
 import { getUserProfile, sendLoginRequest } from "@/services/auth/auth";
-import NextAuth from "next-auth";
 import type { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
